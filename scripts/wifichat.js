20f94c2200ff7f1f6a85562477890510ff494420
Bryan Garza <brygarza@gmail.com>
2014-10-04 06:59:25 -0700

    move js file to their own dir

---
+++ scripts/wifichat.js
@@ -0,0 +1,22 @@
+require (["firebase", "jquery"],
+function ( firebase,   $ ) {
+
+var myDataRef = new Firebase('https://wifichat.firebaseio.com/');
+$('#messageInput').keypress(function (e) {
+    if (e.keyCode == 13) {
+    var name = $('#nameInput').val();
+    var text = $('#messageInput').val();
+    myDataRef.push({name: name, text: text});
+    $('#messageInput').val('');
+    }
+    });
+myDataRef.on('child_added', function(snapshot) {
+    var message = snapshot.val();
+    displayChatMessage(message.name, message.text);
+    });
+function displayChatMessage(name, text) {
+  $('<div/>').text(text).prepend($('<em/>').text(name+': ')).appendTo($('#messagesDiv'));
+  $('#messagesDiv')[0].scrollTop = $('#messagesDiv')[0].scrollHeight;
+};
+
+});
