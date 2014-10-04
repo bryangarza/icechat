95a362fdd812823fcfc784ce82e92b92b0fb3c81
Bryan Garza <brygarza@gmail.com>
2014-10-04 07:16:26 -0700

    fix indentation/spacing

--- scripts/wifichat.js
+++ scripts/wifichat.js
@@ -2,6 +2,7 @@ require (["firebase", "jquery"],
 function ( firebase,   $ ) {
 
 var myDataRef = new Firebase('https://wifichat.firebaseio.com/');
+
 $('#messageInput').keypress(function (e) {
     if (e.keyCode == 13) {
     var name = $('#nameInput').val();
@@ -9,11 +10,13 @@ $('#messageInput').keypress(function (e) {
     myDataRef.push({name: name, text: text});
     $('#messageInput').val('');
     }
-    });
+});
+
 myDataRef.on('child_added', function(snapshot) {
     var message = snapshot.val();
     displayChatMessage(message.name, message.text);
-    });
+});
+
 function displayChatMessage(name, text) {
   $('<div/>').text(text).prepend($('<em/>').text(name+': ')).appendTo($('#messagesDiv'));
   $('#messagesDiv')[0].scrollTop = $('#messagesDiv')[0].scrollHeight;
